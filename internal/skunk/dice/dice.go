9d627e18214f3259ab8b4516044688fed3f5b4de
Esten Rye <esten@ryezone.net>
2020-03-10 00:01:11 -0500

    Add dice class.  need to add tests.

---
+++ internal/skunk/dice/dice.go
@@ -0,0 +1,74 @@
+package dice
+
+import (
+	"github.com/estenrye/skunk/internal/skunk/die"
+)
+
+// Dice Implements a pair of die that are pseudorandom.
+type Dice struct {
+	lastRoll int
+	die1     die.ISkunkDie
+	die2     die.ISkunkDie
+}
+
+// NewDice is the default initializer for Dice.
+func NewDice() ISkunkDice {
+	return NewDiceFromISkunkDice(die.NewDie(), die.NewDie())
+}
+
+// NewDiceFromISkunkDice is an initializer that enables injection of alternative die implementations.
+func NewDiceFromISkunkDice(die1 die.ISkunkDie, die2 die.ISkunkDie) ISkunkDice {
+	return &Dice{
+		die1: die1,
+		die2: die2,
+	}
+}
+
+// GetLastDie1 returns the state of die1 to the caller.
+func (d Dice) GetLastDie1() die.State {
+	return d.die1.GetLastRoll()
+}
+
+// GetLastDie2 returns the state of die2 to the caller.
+func (d Dice) GetLastDie2() die.State {
+	return d.die2.GetLastRoll()
+}
+
+// GetLastState evaluates the states of die1 and die 2 returns the state of the dice roll.
+func (d Dice) GetLastState() State {
+	if d.GetLastDie1() == die.Skunk && d.GetLastDie2() == die.Skunk {
+		return DoubleSkunk
+	}
+
+	if d.GetLastDie1() == die.Skunk {
+		if d.GetLastDie2() == die.Deuce {
+			return SkunkDeuce
+		}
+		return SingleSkunk
+	}
+
+	if d.GetLastDie2() == die.Skunk {
+		if d.GetLastDie1() == die.Deuce {
+			return SkunkDeuce
+		}
+		return SingleSkunk
+	}
+
+	return ScorableRoll
+}
+
+// Roll implements the roll action for a pair of dice
+func (d *Dice) Roll() {
+	d.die1.Roll()
+	d.die2.Roll()
+	d.lastRoll = d.GetLastDie1().ToInt() + d.GetLastDie2().ToInt()
+
+	if d.GetLastDie1() == die.Skunk || d.GetLastDie2() == die.Skunk {
+		d.lastRoll = 0
+	}
+}
+
+// GetLastRoll returns the score of the last dice roll.
+func (d Dice) GetLastRoll() int {
+	return d.lastRoll
+}
