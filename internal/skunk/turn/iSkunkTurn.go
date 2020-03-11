9c771101405e4e6fe4155e6bc2fb745286f11682
Esten Rye <esten.rye@ryezone.com>
2020-03-11 00:24:52 -0500

    Initial data structures for Turn.

---
+++ internal/skunk/turn/iSkunkTurn.go
@@ -0,0 +1,10 @@
+package turn
+
+// ISkunkTurn provides an interface to interact with a turn object
+type ISkunkTurn interface {
+	Roll()
+	Pass()
+	GetScore() int
+	GetPenalty() int
+	GetState() State
+}
---
+++ internal/skunk/turn/turnState.go
@@ -0,0 +1,33 @@
+package dice
+
+// State represents the state of a pair of dice in the game of skunk.
+type State int
+
+const (
+	// NotStarted represents a turn state where the dice have not yet been rolled.
+	NotStarted State = 0
+	// Active represents a turn state where the user has decided to Roll as their first action.
+	Active State = 1
+	// Complete represents a turn state where the user passes their turn before they roll a skunk.
+	Complete State = 2
+	// Complete represents a turn state where the user passes their turn before they roll a skunk.
+	CompleteResetScore State = 3
+	// UnknownState represents a dice state where the dice have not yet been rolled.
+	UnknownState State = 4
+)
+
+func (state State) String() string {
+	names := [...]string{
+		"Not Started",
+		"Active",
+		"Complete",
+		"Complete Reset Score",
+		"Unknown State",
+	}
+
+	if state < NotStarted || state > CompleteResetScore {
+		return "Unknown State"
+	}
+
+	return names[state]
+}
