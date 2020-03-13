eaa88944ee7ee6cde1338bf3da586d4a2f1f111c
Esten Rye <esten@ryezone.net>
2020-03-12 21:10:34 -0500

    refactor test_turn.  add new test for pass.

--- internal/skunk_test/turntest/turn_test.go
+++ internal/skunk_test/turntest/turn_test.go
@@ -1,6 +1,7 @@
 package turntest
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/estenrye/skunk/internal/skunk/dice"
@@ -8,6 +9,20 @@ import (
 	"github.com/estenrye/skunk/internal/skunk/turn"
 	"github.com/estenrye/skunk/internal/skunk_test/dietest"
 )
+
+func assertTurn(t *testing.T, when string,
+	expectedScore int, expectedPenalty int, expectedState turn.State, playerTurn turn.ISkunkTurn,
+) {
+	if playerTurn.GetScore() != expectedScore {
+		t.Errorf("When %s, expected score to be %d, got %d", when, expectedScore, playerTurn.GetScore())
+	}
+	if playerTurn.GetPenalty() != expectedPenalty {
+		t.Errorf("When %s, expected penalty to be %d, got %d", when, expectedPenalty, playerTurn.GetPenalty())
+	}
+	if playerTurn.GetState() != expectedState {
+		t.Errorf("When %s, expected state to be %s, got %s", when, expectedState, playerTurn.GetState())
+	}
+}
 
 func Test_when_a_single_skunk_is_rolled_score_is_zero_penalty_is_one_and_state_is_complete(t *testing.T) {
 	for i := 3; i <= 6; i++ {
@@ -17,17 +32,7 @@ func Test_when_a_single_skunk_is_rolled_score_is_zero_penalty_is_one_and_state_i
 		playerTurn := turn.NewTurnFromISkunkDice(skunkRoll)
 		playerTurn.Roll()
 
-		if playerTurn.GetScore() != 0 {
-			t.Errorf("When a single skunk is rolled, expected score to be 0, got %d", playerTurn.GetScore())
-		}
-
-		if playerTurn.GetPenalty() != 1 {
-			t.Errorf("When a single skunk is rolled, expected penalty to be 1, got %d", playerTurn.GetPenalty())
-		}
-
-		if playerTurn.GetState() != turn.Complete {
-			t.Errorf("When a single skunk is rolled, expected state to be Complete, got %s", playerTurn.GetState())
-		}
+		assertTurn(t, "a single skunk is rolled", 0, 1, turn.Complete, playerTurn)
 	}
 }
 
@@ -38,17 +43,7 @@ func Test_when_a_skunk_deuce_is_rolled_score_is_zero_penalty_is_two_and_state_is
 	playerTurn := turn.NewTurnFromISkunkDice(skunkRoll)
 	playerTurn.Roll()
 
-	if playerTurn.GetScore() != 0 {
-		t.Errorf("When a skunk deuce is rolled, expected score to be 0, got %d", playerTurn.GetScore())
-	}
-
-	if playerTurn.GetPenalty() != 2 {
-		t.Errorf("When a skunk deuce is rolled, expected penalty to be 2, got %d", playerTurn.GetPenalty())
-	}
-
-	if playerTurn.GetState() != turn.Complete {
-		t.Errorf("When a skunk deuce is rolled, expected state to be Complete, got %s", playerTurn.GetState())
-	}
+	assertTurn(t, "a skunk deuce is rolled", 0, 2, turn.Complete, playerTurn)
 }
 
 func Test_when_a_double_skunk_is_rolled_score_is_zero_penalty_is_four_and_state_is_completeResetScore(t *testing.T) {
@@ -58,17 +53,7 @@ func Test_when_a_double_skunk_is_rolled_score_is_zero_penalty_is_four_and_state_
 	playerTurn := turn.NewTurnFromISkunkDice(skunkRoll)
 	playerTurn.Roll()
 
-	if playerTurn.GetScore() != 0 {
-		t.Errorf("When a double skunk is rolled, expected score to be 0, got %d", playerTurn.GetScore())
-	}
-
-	if playerTurn.GetPenalty() != 4 {
-		t.Errorf("When a double skunk is rolled, expected penalty to be 4, got %d", playerTurn.GetPenalty())
-	}
-
-	if playerTurn.GetState() != turn.CompleteResetScore {
-		t.Errorf("When a double skunk is rolled, expected state to be Complete Reset Score, got %s", playerTurn.GetState())
-	}
+	assertTurn(t, "a double skunk is rolled", 0, 4, turn.CompleteResetScore, playerTurn)
 }
 
 func Test_when_no_skunk_is_rolled_score_is_sum_penalty_is_zero_and_state_is_active(t *testing.T) {
@@ -81,17 +66,10 @@ func Test_when_no_skunk_is_rolled_score_is_sum_penalty_is_zero_and_state_is_acti
 			playerTurn.Roll()
 			playerTurn.Roll()
 
-			if playerTurn.GetScore() != 2*(i+j) {
-				t.Errorf("When a %s and a %s are rolled twice, expected score to be %d, got %d", die.State(i), die.State(j), 2*(i+j), playerTurn.GetScore())
-			}
-
-			if playerTurn.GetPenalty() != 0 {
-				t.Errorf("When a %s and a %s are rolled twice, expected penalty to be 0, got %d", die.State(i), die.State(j), playerTurn.GetPenalty())
-			}
-
-			if playerTurn.GetState() != turn.Active {
-				t.Errorf("When a %s and a %s are is rolled twice, expected state to be Active, got %s", die.State(i), die.State(j), playerTurn.GetState())
-			}
+			assertTurn(t,
+				fmt.Sprintf("a %s and a %s are rolled twice", die.State(i), die.State(j)),
+				2*(i+j), 0, turn.Active,
+				playerTurn)
 		}
 	}
 }
@@ -102,13 +80,15 @@ func Test_when_no_actions_have_been_taken_state_is_NotStarted_score_is_zero_pena
 	skunkRoll := dice.NewDiceFromISkunkDice(die1, die2)
 	playerTurn := turn.NewTurnFromISkunkDice(skunkRoll)
 
-	if playerTurn.GetScore() != 0 {
-		t.Errorf("When no actions have been taken, expected score to be 0, got %d", playerTurn.GetScore())
-	}
-	if playerTurn.GetPenalty() != 0 {
-		t.Errorf("When no actions have been taken, expected penalty to be 0, got %d", playerTurn.GetPenalty())
-	}
-	if playerTurn.GetState() != turn.NotStarted {
-		t.Errorf("When no actions have been taken, expected state to be Not Started, got %s", playerTurn.GetState())
-	}
+	assertTurn(t, "no actions have been taken", 0, 0, turn.NotStarted, playerTurn)
+}
+
+func Test_when_pass_is_taken_as_first_action_score_is_zero_penalty_is_zero_state_is_complete(t *testing.T) {
+	die1 := dietest.NewDieFromDieState(die.Skunk)
+	die2 := dietest.NewDieFromDieState(die.Skunk)
+	skunkRoll := dice.NewDiceFromISkunkDice(die1, die2)
+	playerTurn := turn.NewTurnFromISkunkDice(skunkRoll)
+	playerTurn.Pass()
+
+	assertTurn(t, "player passes on the first turn", 0, 0, turn.Complete, playerTurn)
 }
