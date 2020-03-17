c5cd7ec502729152380c7f286e3c8b087b8a60d8
Esten Rye <esten.rye@ryezone.com>
2020-03-17 02:37:37 -0500

    add more skunk_app test coverage

--- cmd/skunk_app/skunk_app.go
+++ cmd/skunk_app/skunk_app.go
@@ -9,31 +9,22 @@ import (
 	"github.com/estenrye/skunk/internal/skunk/player"
 )
 
-func getGreeting() string {
-	return "Hello World!"
-}
-
-func getTurnInput(p player.ISkunkPlayer, reader *bufio.Reader) {
+func getTurnInput(p player.ISkunkPlayer, text string) {
 	fmt.Printf("%s: Current Turn Score: %d.  Would you like to roll or pass? (r|p): ", p.GetName(), p.GetLastTurn().GetScore())
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		// convert CRLF to LF
-		text = strings.Replace(text, "\n", "", -1)
-		text = strings.Replace(text, "\r", "", -1)
-		switch text {
-		case "r":
-			p.Roll()
-			r := p.GetLastTurn().GetLastRoll()
-			fmt.Printf("%s: Rolled a %s and a %s.\n", p.GetName(), r.GetLastDie1(), r.GetLastDie2())
-			break
-		case "p":
-			p.Pass()
-			break
-		default:
-			break
-		}
+	// convert CRLF to LF
+	text = strings.Replace(text, "\n", "", -1)
+	text = strings.Replace(text, "\r", "", -1)
+	switch text {
+	case "r":
+		p.Roll()
+		r := p.GetLastTurn().GetLastRoll()
+		fmt.Printf("%s: Rolled a %s and a %s.\n", p.GetName(), r.GetLastDie1(), r.GetLastDie2())
+		break
+	case "p":
+		p.Pass()
+		break
+	default:
+		break
 	}
 }
 
@@ -47,7 +38,12 @@ func main() {
 		fmt.Printf("It's %s's turn.\n", p.GetName())
 
 		for p.GetLastState() != player.CompleteTurn && p.GetLastState() != player.CompleteEndgame {
-			getTurnInput(p, reader)
+			text, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				getTurnInput(p, text)
+			}
 		}
 	}
 	fmt.Printf("%s: %d points, %d chips\n", p.GetName(), p.GetLastScore(), p.GetLastChips())
--- cmd/skunk_app/skunk_app_test.go
+++ cmd/skunk_app/skunk_app_test.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"testing"
+
+	"github.com/estenrye/skunk/internal/skunk/player"
 )
 
-func TestMainGreeting(t *testing.T) {
-	var expected = "Hello World!"
-	var actual = getGreeting()
-	if actual != expected {
-		t.Errorf("getGreeting() returned incorrect greeting.")
+func Test_getTurnInput(t *testing.T) {
+	p := player.NewPlayer("playerOne")
+	p.NewTurn()
+
+	getTurnInput(p, "a\r\n")
+	if p.GetLastState() != player.TurnNotStarted {
+		t.Errorf("Expected input of string value other than 'r' or 'p' to noop, player state: %s.", p.GetLastState())
+	}
+
+	p.Roll()
+	if p.GetLastState() == player.TurnNotStarted {
+		t.Errorf("Expected input of string value 'r' to change player state, got %s.", p.GetLastState())
+	}
+
+	p = player.NewPlayer("playerOne")
+	p.NewTurn()
+	p.Pass()
+	if p.GetLastState() != player.CompleteTurn {
+		t.Errorf("Expected input of string value 'p' to change player state to 'Complete Turn', got %s", p.GetLastState())
 	}
 }
